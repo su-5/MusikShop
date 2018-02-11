4c89cd243f620ec6f5a8e683650150a6eb3f2173
dima-tkachenko <dima-tkachenko@tut.by>
2018-02-11 21:50:26 +0300

    Добавлен модуль DAL

--- ShopOfMusicalInstruments/ShopOfMusicalInstruments/App_Start/BundleConfig.cs
+++ ShopOfMusicalInstruments/ShopOfMusicalInstruments/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace ShopOfMusicalInstruments
     {
         // Дополнительные сведения об объединении см. на странице https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
-        {
+        {  
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
