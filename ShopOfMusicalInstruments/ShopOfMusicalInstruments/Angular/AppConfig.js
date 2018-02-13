28dd97f6c08ee57b0a9be0e2e7a8488f8bdced4d
dima-tkachenko <dima-tkachenko@tut.by>
2018-02-14 00:53:06 +0300

    Добавление API контроллера и GenericRepository

--- ShopOfMusicalInstruments/ShopOfMusicalInstruments/Angular/AppConfig.js
+++ ShopOfMusicalInstruments/ShopOfMusicalInstruments/Angular/AppConfig.js
@@ -5,12 +5,12 @@
         .module("Web")
         .config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function ($stateProvider, $urlRouterProvider, $locationProvider) {
             // Index
-            $stateProvider.state("mainPage/Catalog", {
+            $stateProvider.state("mainPage/catalog", {
                 url: "/catalog",
                 templateUrl: "Angular/Views/Catalog.html",
-                controller: "CatalogController"
+                controller: "catalogController"
             });
 
-            $urlRouterProvider.otherwise("/Catalog");
+            $urlRouterProvider.otherwise("/catalog");
         }]);
 })();
