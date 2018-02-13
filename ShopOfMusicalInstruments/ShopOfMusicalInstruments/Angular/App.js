5dbf89cdb9dfe914d6835f24a244e0362c9910fd
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2018-02-14 00:55:49 +0300

    Рефакторинг

--- ShopOfMusicalInstruments/ShopOfMusicalInstruments/Angular/App.js
+++ ShopOfMusicalInstruments/ShopOfMusicalInstruments/Angular/App.js
@@ -9,13 +9,13 @@
 
     app.run(["$rootScope", "$location", "$http", "$state", "$stateParams", "$sce", "loadingService",
         function ($rootScope, $location, $http, $state, $stateParams, $sce, loadingService) {
-            $rootScope.loadingShow = function () {
-                $rootScope.loadingIsShow = loadingService.show(); // loading
-            };
+            //$rootScope.loadingShow = function () {
+            //    $rootScope.loadingIsShow = loadingService.show(); // loading
+            //};
 
-            $rootScope.loadingHide = function () {
-                $rootScope.loadingIsShow = loadingService.hide();
-            };
+           // $rootScope.loadingHide = function () {
+               // $rootScope.loadingIsShow = loadingService.hide();
+         //   };
         }
     ]);
 
