59ee7bcd0b5d2b80fb7a0bd5a5b3a87d4cc96cf2
dima-tkachenko <dima-tkachenko@tut.by>
2018-02-17 02:57:21 +0300

    Не работает контроллер

--- ShopOfMusicalInstruments/DAL.Core/ModelDB.Context.cs
+++ ShopOfMusicalInstruments/DAL.Core/ModelDB.Context.cs
@@ -30,7 +30,7 @@ namespace DAL.Core
         public virtual DbSet<Country> Countries { get; set; }
         public virtual DbSet<NumberString> NumberStrings { get; set; }
         public virtual DbSet<Picture> Pictures { get; set; }
-        public virtual DbSet Products { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Subcategory> Subcategories { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<User> Users { get; set; }
--- ShopOfMusicalInstruments/ShopOfMusicalInstruments/Controllers/ProductController.cs
+++ ShopOfMusicalInstruments/ShopOfMusicalInstruments/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Results;
 using DAL.Core;
 using DAL.Core.ModelDTO;
 
