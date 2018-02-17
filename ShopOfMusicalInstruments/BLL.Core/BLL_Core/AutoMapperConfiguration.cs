409e364ee93a8597e73633d454de009d1869789e
dima-tkachenko <dima-tkachenko@tut.by>
2018-02-17 11:34:49 +0300

    Добавление сопоставления классов

---
+++ ShopOfMusicalInstruments/BLL.Core/BLL_Core/AutoMapperConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BLL.Core.BLL_Core.Mapping;
+
+namespace BLL.Core.BLL_Core
+{
+    class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(config: x => x.AddProfile<CatalogMapper>());
+        }
+    }
+}
---
+++ ShopOfMusicalInstruments/BLL.Core/BLL_Core/Mapping/CatalogMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DAL.Core;
+using DAL.Core.ModelDTO;
+
+namespace BLL.Core.BLL_Core.Mapping
+{
+    public class CatalogMapper : Profile
+    {
+        public CatalogMapper()
+        {
+            CreateMap<Product, ProductDTO>()
+            .ForMember(d => d.Id, opts => opts.MapFrom(src => src.Id))
+            .ForMember(d => d.Name, opts => opts.MapFrom(src => src.Name))
+            .ForMember(d => d.CountryId, opts => opts.MapFrom(src => src.CountryId))
+            .ForMember(d => d.PictureId, opts => opts.MapFrom(src => src.PictureId))
+            .ForMember(d => d.BrandId, opts => opts.MapFrom(src => src.BrandId))
+            .ForMember(d => d.NumberStringId, opts => opts.MapFrom(src => src.NumberStringId))
+            .ForMember(d => d.NumberProduct, opts => opts.MapFrom(src => src.NumberProduct))
+            .ForMember(d => d.Price, opts => opts.MapFrom(src => src.Price))
+            .ForMember(d => d.DateManufacture, opts => opts.MapFrom(src => src.DateManufacture))
+            .ForMember(d => d.DateCreate, opts => opts.MapFrom(src => src.DateCreate))
+            .ForMember(d => d.DateUpdate, opts => opts.MapFrom(src => src.DateUpdate))
+            .ForMember(d => d.DateDelete, opts => opts.MapFrom(src => src.DateDelete));
+        }
+    }
+}
