f2b9a7316a725723ac346ec248d44bed7b6898fe
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2018-02-23 22:16:18 +0300

    test

--- ShopOfMusicalInstruments/DAL.Core/GenericRepository/GenericRepository.cs
+++ ShopOfMusicalInstruments/DAL.Core/GenericRepository/GenericRepository.cs
@@ -121,14 +121,14 @@ namespace DAL.Core.GenericRepository
         }
 
 
-        public IQueryable<T> GetAllWithPaging<TKey>(int pageSize, int page, out int total, Expression<Func<T, TKey>> orderBy, bool isOrderAsc, Expression<Func<T, bool>> filter = null)
-        {
-            IQueryable<T> resultFromDb = _dbSet.Where(filter ?? throw new ArgumentNullException(nameof(filter)));
-            total = resultFromDb.Count();
+        //public IQueryable<T> GetAllWithPaging<TKey>(int pageSize, int page, out int total, Expression<Func<T, TKey>> orderBy, bool isOrderAsc, Expression<Func<T, bool>> filter = null)
+        //{
+        //    //IQueryable<T> resultFromDb = _dbSet.Where(filter ?? throw new ArgumentNullException(nameof(filter)));
+        //    //total = resultFromDb.Count();
 
-            IQueryable<T> resultForPaging = isOrderAsc ? resultFromDb.OrderBy(orderBy).Skip((page - 1) * pageSize).Take(pageSize) : resultFromDb.OrderByDescending(orderBy).Skip((page - 1) * pageSize).Take(pageSize);
-            return resultForPaging;
-        }
+        //    //IQueryable<T> resultForPaging = isOrderAsc ? resultFromDb.OrderBy(orderBy).Skip((page - 1) * pageSize).Take(pageSize) : resultFromDb.OrderByDescending(orderBy).Skip((page - 1) * pageSize).Take(pageSize);
+        //    //return resultForPaging;
+        //}
     }
 
 }
--- ShopOfMusicalInstruments/DAL.Core/GenericRepositoryModel/Repository/ProductDAL.cs
+++ ShopOfMusicalInstruments/DAL.Core/GenericRepositoryModel/Repository/ProductDAL.cs
@@ -1,4 +1,7 @@
-﻿using DAL.Core.DAL_Core;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using DAL.Core.DAL_Core;
 using DAL.Core.GenericRepository;
 using DAL.Core.GenericRepositoryModel.Interfaces;
 
@@ -8,6 +11,12 @@ namespace DAL.Core.GenericRepositoryModel.Repository
    {
        public ProductDAL(IDbFactory dbFactory) : base(dbFactory)
        {
+       }
+
+       public IQueryable<Product> GetAllWithPaging<TKey>(int pageSize, int page, out int total, Expression<Func<Product, TKey>> orderBy, bool isOrderAsc,
+           Expression<Func<Product, bool>> filter = null)
+       {
+           throw new NotImplementedException();
        }
    }
 }
